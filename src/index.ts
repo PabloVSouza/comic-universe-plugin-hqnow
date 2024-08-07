65723320b6b4e8bf7418145da28d79921a6f0aaf
Pablo Souza <pablovsouza@gmail.com>
2024-08-07 16:05:41 +0100

    Implement hqnow

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,114 @@
-class ExampleRepoPlugin implements IRepoPluginRepository {
-  public RepoName = 'Example Repo Plugin'
-  public RepoTag = 'example'
-  public RepoUrl = 'https://example.com/'
+import { ApolloClient, gql, HttpLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+import fetch from 'cross-fetch'
 
-  constructor(data: IRepoPluginRepositoryInit) {}
+class HQNowRepoPlugin implements IRepoPluginRepository {
+  public RepoName = 'HQ Now'
+  public RepoTag = 'hqnow'
+  public RepoUrl = 'https://admin.hq-now.com/graphql'
+  private client: ApolloClient<NormalizedCacheObject>
+
+  constructor() {
+    const cache: InMemoryCache = new InMemoryCache({
+      addTypename: false
+    })
+    this.client = new ApolloClient({
+      cache,
+      link: new HttpLink({ uri: this.RepoUrl, fetch })
+    })
+  }
 
   public methods: IRepoPluginMethods = {
-    getList: async (): Promise<ComicInterface[]> => {
-      return new Promise((resolve) => {
-        resolve([])
-      })
-    },
+    getList: async (): Promise<ComicInterface[]> => this.methods.search({ search: 'A' }),
 
     search: async ({ search }): Promise<ComicInterface[]> => {
-      return new Promise((resolve) => {
-        resolve([])
-      })
+      const query = {
+        query: gql`
+          query getHqsByName($search: String!) {
+            getHqsByName(name: $search) {
+              siteId: id
+              name
+              synopsis
+              status
+            }
+          }
+        `,
+        variables: { search }
+      }
+
+      try {
+        const { data } = await this.client.query(query)
+        return new Promise((resolve) => {
+          resolve(data.getHqsByName as ComicInterface[])
+        })
+      } catch (_e) {
+        return new Promise((resolve) => {
+          resolve([])
+        })
+      }
     },
 
     getDetails: async (search): Promise<Partial<ComicInterface>> => {
+      const { siteId } = search
+      const { data } = await this.client.query({
+        query: gql`
+          query getHqsById($id: Int!) {
+            getHqsById(id: $id) {
+              cover: hqCover
+              publisher: publisherName
+            }
+          }
+        `,
+        variables: { id: Number(siteId) }
+      })
+
+      const res = {
+        ...data.getHqsById[0],
+        siteId: String(siteId),
+        type: 'hq'
+      }
+
       return new Promise((resolve) => {
-        resolve({})
+        resolve(res)
       })
     },
 
     getChapters: async ({ siteId }): Promise<ChapterInterface[]> => {
-      return new Promise((resolve) => {
-        resolve([])
-      })
-    },
+      const query = {
+        query: gql`
+          query getChaptersByHqId($id: Int!) {
+            getChaptersByHqId(hqId: $id) {
+              name
+              number
+              siteId: id
+              pages: pictures {
+                filename: image
+                path: pictureUrl
+              }
+            }
+          }
+        `,
+        variables: { id: Number(siteId) }
+      }
 
-    getPages: async ({ siteLink }) => {
+      const { data } = await this.client.query(query)
+      //@ts-ignore
+      const res = data.getChaptersByHqId.reduce((acc, chapter) => {
+        return [
+          ...acc,
+          {
+            ...chapter,
+            siteId: String(siteId),
+            offline: false,
+            pages: JSON.stringify(chapter.pages)
+          }
+        ]
+      }, []) as ChapterInterface[]
+
       return new Promise((resolve) => {
-        resolve([])
+        resolve(res)
       })
     }
   }
 }
 
-export default ExampleRepoPlugin
+export default HQNowRepoPlugin
